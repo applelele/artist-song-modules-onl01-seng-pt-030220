d8d50e63988cda0aabfa4f50b68d4088ef05dd93
YUKIKO IKEDA <applelele@gmail.com>
2020-04-12 00:03:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,7 @@ require_relative './concerns/paramable.rb'
 
 class Artist
   extend Memorable::ClassMethods
-  include Memorable::instanceMethods, Findable, Paramable
+  include Memorable::InstanceMethods, Findable, Paramable
 
   attr_accessor :name
   attr_reader :songs
