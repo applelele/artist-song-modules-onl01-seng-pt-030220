4afc3180ad0d78ec7dd54a333737c980a478dd2f
YUKIKO IKEDA <applelele@gmail.com>
2020-04-12 00:01:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,16 +6,16 @@ require_relative './concerns/paramable.rb'
 
 class Song
   extend Memorable::ClassMethods
-  include Memorable::instanceMethods
+  include Memorable::instanceMethods, Findable, Paramable
 
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    self.class.all << self #@@songs << self
-  end
+  # def initialize
+  #   self.class.all << self #@@songs << self
+  # end
 
   # def self.find_by_name(name)
   #   @@songs.detect{|a| a.name == name}
